0de4b4e089bf9a6419730e1a25eac62f5205ec52
cmhull42 <cmhull42@gmail.com>
2015-04-13 10:57:07 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import sqlite3
+from flask import Flask, g
+from contextlib import closing
+
+app = Flask(__name__)
+app.config.from_object("config")
+from app import views
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource("schema.sql", mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
---
+++ config.py
@@ -0,0 +1,5 @@
+DATABASE = '/tmp/paste.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
